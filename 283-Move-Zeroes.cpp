d5e39614e5afa1e79a840c781675ce6ca474294c
yash swarnkar <ys703981@gmail.com>
2026-09-23 12:37:06 +0530

    [283] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
-            }
+        int n = nums.size();
+        vector<int> temp;
+        for(int i=0; i<n; i++){
+              if(nums[i] != 0 ){
+                temp.push_back(nums[i]);
+              }
+        }  
+        int nz = temp.size();
+        for(int i = 0; i<nz; i++){
+            nums[i] = temp[i];
         }
-
-        while (j < nums.size()) {
-            nums[j] = 0;
-            j++;
+        for(int i = nz; i<n; i++){
+            nums[i] = 0;
         }
-    }
+          }
 };
